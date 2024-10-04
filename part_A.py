a1197d7a440af49e8d06d1857d7a9483a95badc3
Isa Klokk <isa.botnen-klokk@student.nhh.no>
2024-10-04 14:12:32 +0200

    final version

--- part_A.py
+++ part_A.py
@@ -1,35 +1,58 @@
 """
 TECH2 mandatory assignment - Part A
 
-Write the implementation of part A of the exercise below.
 """
+from math import sqrt
 
-def std_loops(x):
-    """
-    Compute standard deviation of x using loops.
+num_lst = [1, 2, 3 , 4, 5]
+x = num_lst
 
-    Parameters
-    ----------
-    x: Sequence of numbers
+def std_loops(x): 
+    N = 0
+    for _ in x: # Calculating the length of the list manually
+        N += 1
 
-    Returns
-    -------
-    sd : float
-        Standard deviation of the list of numbers.
-    """
+    total_sum = 0
+    for i in x: # Calculating the mean of x
+        total_sum += i
+    mean_x = total_sum / N # The mean of x
+
+    sum_S = 0
+    for i in x: # Calculating the mean of squares
+        sum_S += i ** 2
+        
+    mean_S = sum_S / N # The mean of squares
+    variance = mean_S - (mean_x ** 2) # The variance
+    S_deviation = sqrt(variance) # The standard deviation
+
+    return S_deviation
+
+print('The standard deviation with alternative 1:', std_loops(x))
+
+
 
 def std_builtin(x):
-    """
-    Compute standard deviation of x using the built-in functions sum()
-    and len().
+    N = len(x)
 
-    Parameters
-    ----------
-    x: Sequence of numbers
-
-    Returns
-    -------
-    sd : float
-        Standard deviation of the list of numbers.
-    """
+    mean_x = sum(x) / N # Calculating the mean of x
     
+    sum_S = sum(i**2 for i in x) 
+    mean_S = sum_S / N # Calculating the mean of squares
+
+    variance = mean_S - mean_x ** 2 # Calculating variance
+    s_deviation = sqrt(variance) # Calculating the standard deviation
+    return s_deviation
+
+print('The standard deviation with alternative 2:', std_builtin(x))
+
+
+import numpy as np
+
+std_np = np.std(x) # Calculating the standard deviation
+print('The standard deviation with alternative 3:', std_np)
+
+# Compare all three methods
+if round(std_loops(x), 5) == round(std_builtin(x), 5) == round(std_np, 5):
+    print('All three methods calculate the same standard deviations for num_lst')
+else:
+    print('There is a difference in the standard deviation calculations')
